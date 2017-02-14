fd6857ef43d7b82b17d3e96d78c7f379ba324e22
Darío Rodríguez <dariorodriguezdiaz8@gmail.com>
2017-02-14 20:05:02 +0100

    Anadido Boton reload y pinta usuarios de la API

--- app/src/main/java/paisdeyann/floway/FragmentsTabs/MapViewFragment.java
+++ app/src/main/java/paisdeyann/floway/FragmentsTabs/MapViewFragment.java
@@ -56,7 +56,7 @@ import static android.content.Context.LOCATION_SERVICE;
  * Created by Dario on 11/02/2017.
  */
 
-public class MapViewFragment extends Fragment implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, GoogleMap.OnMarkerClickListener {
+public class MapViewFragment extends Fragment implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, GoogleMap.OnMarkerClickListener {
 
     MapView mMapView;
     private GoogleMap mgoogleMap;
@@ -70,14 +70,23 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback, Goo
     LocationManager locationManager;
     Menu_Principal activity;
 
+    double radio = 10;
+    int conductor = 1;
+    int conectado = 1;
+
+    //Listener de la ubicacion
     LocationListener mLocationListener = new LocationListener() {
         @Override
         public void onLocationChanged(final Location location) {
             tuLatitud = location.getLatitude();
             tuLongitud = location.getLongitude();
-            setMarkersPasajeros();
+            zoom();
+            pintaUsuarios(tuLatitud,tuLongitud,radio,conductor,conectado);
+
+
             Log.v("syso","latitud y longitud "+tuLatitud+" , "+tuLongitud);
         }
+
         @Override
         public void onStatusChanged(String provider, int status, Bundle extras) {
         }
@@ -99,7 +108,19 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback, Goo
         mMapView = (MapView) rootView.findViewById(R.id.mapView);
         mMapView.onCreate(savedInstanceState);
 
-        mMapView.onResume(); // needed to get the map to display immediately
+        mMapView.onResume(); // needed to get the map to display immediate
+
+        Button reload = (Button) rootView.findViewById(R.id.button5);
+
+        reload.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              zoom();
+              pintaUsuarios(tuLatitud,tuLongitud,radio,conductor,conectado);
+          }
+      });
+
+
 
         try {
             MapsInitializer.initialize(getActivity().getApplicationContext());
@@ -113,27 +134,11 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback, Goo
                 mgoogleMap = mMap;
                 addInfo();
                 getUbicacion();
-
-                double radio = 10;
-               // double latitud = 39.590381 ;
-               // double longitud = -0.533108;
-                int conductor = 1;
-                int conectado = 1;
-
-                Object[] objetos = new Object[6];
-                objetos[0] = radio;      // radio   double
-                objetos[1] = tuLatitud;       // latitud  atributo clase principal  double
-                objetos[2] = tuLongitud;     //longitud   atributo clase principal   double
-                objetos[3] = conductor;             // conductor    int 1 conductor 0 pasajero
-                objetos[4] = conectado;             // conectado    int 1 conectado 0 desconectado
-                objetos[5] = esteActivity;          // este fragment
-
-                ConseguirUsuariosPorRadio threadConseguirUsuarios = new ConseguirUsuariosPorRadio();
-
-                threadConseguirUsuarios.execute(objetos);
-
-
                 setMarkersPasajeros();
+                zoom();
+                pintaUsuarios(tuLatitud,tuLongitud,radio,conductor,conectado);
+
+
             }
         });
 
@@ -142,15 +147,7 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback, Goo
 
         return rootView;
     }
-    protected boolean isLocationEnabled(){
-        String le = LOCATION_SERVICE;
-        locationManager = (LocationManager) context.getSystemService(le);
-        if(!locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
-            return false;
-        } else {
-            return true;
-        }
-    }
+
 
     public void setContext(Context con, Menu_Principal me){
         context = con;
@@ -159,7 +156,7 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback, Goo
 
     public void getUbicacion(){
 
-        long minTime=100;
+        long minTime=10;
         float minDistance=100;
         locationManager = (LocationManager) activity.getSystemService(LOCATION_SERVICE);
 
@@ -193,7 +190,15 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback, Goo
             return true;
         }
     }
-
+    protected boolean isLocationEnabled(){
+        String le = LOCATION_SERVICE;
+        locationManager = (LocationManager) context.getSystemService(le);
+        if(!locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+            return false;
+        } else {
+            return true;
+        }
+    }
     public void setMarkersPasajeros(){
 
         if(!isLocationEnabled()) {
@@ -216,33 +221,31 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback, Goo
             AlertDialog alert = builder.create();
             alert.show();
         }
-        mgoogleMap.clear();
+        //mgoogleMap.clear();
 
+    }
 
-
+    public void zoom(){
         // For dropping a marker at a point on the Map
         LatLng marker = new LatLng(this.tuLatitud, this.tuLongitud);
-
-
-
-        MarkerOptions markerMaps = new MarkerOptions()
-                .position(marker)
-                .title("Yo")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.android_pasajero))
-                .snippet("Mi posicion");
-
-
-
-        mgoogleMap.addMarker(markerMaps);
-        mgoogleMap.setOnMarkerClickListener(this);
-
-
 
 
         // For zooming automatically to the location of the marker
         CameraPosition cameraPosition = new CameraPosition.Builder().target(marker).zoom(12).build();
         mgoogleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
 
+    public void pintaUsuarios(double la,double lo,double ra,int con, int pas){
+        mgoogleMap.clear();
+        ConseguirUsuariosPorRadio c = new ConseguirUsuariosPorRadio();
+        Object[] objetos = new Object[6];
+        objetos[0] = ra;      // radio   double
+        objetos[1] = la;       // latitud  atributo clase principal  double
+        objetos[2] = lo;     //longitud   atributo clase principal   double
+        objetos[3] = con;             // conductor    int 1 conductor 0 pasajero
+        objetos[4] = pas;             // conectado    int 1 conectado 0 desconectado
+        objetos[5] = esteActivity;
+        c.execute(objetos);
 
     }
 
@@ -264,8 +267,6 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback, Goo
 
         if (mgoogleMap != null) {
             mgoogleMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
-
-
                 @Override
                 public View getInfoWindow(Marker marker) {
                     return null;
@@ -274,13 +275,11 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback, Goo
                 @Override
                 public View getInfoContents(Marker marker) {
                     View v = inflater.inflate(R.layout.info_google_window, null);
-
                     ImageView imag = (ImageView) v.findViewById(R.id.imageView1);
                     TextView nom = (TextView) v.findViewById(R.id.tv_locality);
                     TextView lat = (TextView) v.findViewById(R.id.tv_lat);
                     TextView lon = (TextView) v.findViewById(R.id.tv_lng);
                     Button btn = (Button) v.findViewById(R.id.button4);
-
                     return null;
                 }
             });
@@ -288,12 +287,8 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback, Goo
         }
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
 
-
-    }
-
+    //-----------------------------PURRIA---------------------------------------------------------
     @Override
     public void onConnected(@Nullable Bundle bundle) {
     getUbicacion();
@@ -360,82 +355,7 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback, Goo
         this.usuarios.add(usuario);
     }
 
-/*
-    public void cogerUsuarios(){
 
-        ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-
-        if (networkInfo != null && networkInfo.isConnected()) {
-
-            Log.d("prueba","conecto en el maps");
-            getUsers();
-
-        } else {
-            Log.d("prueba","no conecto en el maps");
-        }
-
-
-
-    }
-
-    public void añadirUsuarios(){
-        URL url = null;
-        HttpURLConnection conn;
-        try {
-            String cadenaConexion = "http://54.93.101.246/APIFLOWAY-PHP/apiNueva.php?conectado=1&conductor=1&radio=5&longitud=-0.533108&latitud=39.590381&"+ Conexion.APIKEY;
-            Log.d("prueba","cadena conexion: "+cadenaConexion);
-            url = new URL(cadenaConexion);
-            conn = (HttpURLConnection) url.openConnection();
-            conn.setReadTimeout(10000);
-            conn.setConnectTimeout(15000);
-            conn.setRequestMethod("GET");                   // SuperGlobal de como vas a enviar los datos
-            conn.setDoInput(true);
-            Log.d("prueba","llego 1");
-            conn.connect();
-            Log.d("prueba","llego 2");
-            int response = conn.getResponseCode();		// código devuelto por web Service
-            InputStream is = conn.getInputStream();
-
-            String respuesta = transformarInputStremString(is);
-
-            Log.d("prueba","respuesta: "+respuesta);
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-    }
-
-    private String transformarInputStremString(InputStream is) {
-
-        StringBuilder stringBuilder = new StringBuilder();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-
-        String linea = "";
-
-        try {
-            while((linea = reader.readLine()) != null){
-                stringBuilder.append(linea);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            reader.close();
-            is.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return stringBuilder.toString();
-    }
-*/
 }
 
 
